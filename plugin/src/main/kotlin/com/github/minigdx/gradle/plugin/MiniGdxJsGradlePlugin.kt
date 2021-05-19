aec4a699216abdaaea5976b04eb5f24f423a7984
David Wursteisen <david.wursteisen+git@gmail.com>
2021-05-19 09:45:38 +0200

    unpack resources for specific dependencies

--- plugin/src/main/kotlin/com/github/minigdx/gradle/plugin/MiniGdxJsGradlePlugin.kt
+++ plugin/src/main/kotlin/com/github/minigdx/gradle/plugin/MiniGdxJsGradlePlugin.kt
@@ -7,6 +7,8 @@ import com.github.minigdx.gradle.plugin.internal.maybeCreateMiniGdxExtension
 import com.github.minigdx.gradle.plugin.internal.minigdx
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.attributes.Attribute
+import org.gradle.api.tasks.Copy
 import org.gradle.api.tasks.bundling.Zip
 
 class MiniGdxJsGradlePlugin : Plugin<Project> {
@@ -40,12 +42,36 @@ class MiniGdxJsGradlePlugin : Plugin<Project> {
                 project.logger.lifecycle("[MINIGDX] The js distribution of your game is available at: ${it.outputs.files.first()}")
             }
         }
+
+        // Copy resources from minigdx into the web distribution so it's available, as the web platform
+        // can't access bundled resources.
+        val copy = project.tasks.register("unpack-minigdx-resources", Copy::class.java) {
+            val dependencies = project.configurations.getAt("minigdxToUnpack")
+
+            it.group = "minigdx"
+            it.description = "Unpack resources used by minigdx needed by the web platform."
+            it.from(project.zipTree(dependencies.singleFile))
+            it.include("/internal/**")
+            it.into("build/processedResources/js/main")
+
+        }
+        project.afterEvaluate {
+            project.tasks.getByName("jsProcessResources").dependsOn(copy)
+        }
     }
 
     private fun configureMiniGdxDependencies(project: Project) {
+        project.configurations.create("minigdxToUnpack") {
+            it.setTransitive(false)
+            it.attributes {
+                it.attribute(Attribute.of("org.gradle.usage", String::class.java), "kotlin-runtime")
+            }
+        }
+
         project.afterEvaluate {
             project.dependencies.add("commonMainImplementation", "com.github.minigdx:minigdx:${project.minigdx.version.get()}")
             project.dependencies.add("jsMainImplementation", "com.github.minigdx:minigdx-js:${project.minigdx.version.get()}")
+            project.dependencies.add("minigdxToUnpack", "com.github.minigdx:minigdx-js:${project.minigdx.version.get()}")
         }
     }
 }
