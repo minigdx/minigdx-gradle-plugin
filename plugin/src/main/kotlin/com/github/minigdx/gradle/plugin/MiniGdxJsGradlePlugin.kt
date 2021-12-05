2190d876d2d975551f21c23af4a903497259dd89
David Wursteisen <david.wursteisen+git@gmail.com>
2021-12-05 22:40:38 +0100

    Fix to make the plugin work for the web platform

--- plugin/src/main/kotlin/com/github/minigdx/gradle/plugin/MiniGdxJsGradlePlugin.kt
+++ plugin/src/main/kotlin/com/github/minigdx/gradle/plugin/MiniGdxJsGradlePlugin.kt
@@ -1,41 +1,66 @@
 package com.github.minigdx.gradle.plugin
 
-import com.github.minigdx.gradle.plugin.internal.MiniGdxPlatform
-import com.github.minigdx.gradle.plugin.internal.checkCommonPlugin
-import com.github.minigdx.gradle.plugin.internal.createDir
 import com.github.minigdx.gradle.plugin.internal.maybeCreateMiniGdxExtension
 import com.github.minigdx.gradle.plugin.internal.minigdx
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.attributes.Attribute
 import org.gradle.api.tasks.Copy
 import org.gradle.api.tasks.bundling.Zip
+import org.jetbrains.kotlin.gradle.dsl.KotlinJsProjectExtension
+import java.io.File
+import java.net.URI
 
 class MiniGdxJsGradlePlugin : Plugin<Project> {
 
     override fun apply(project: Project) {
         project.maybeCreateMiniGdxExtension()
-        project.checkCommonPlugin(MiniGdxPlatform.JAVASCRIPT)
+
+        project.apply { plugin("org.jetbrains.kotlin.js") }
+
+
+        configureProjectRepository(project)
+        project.extensions.configure<KotlinJsProjectExtension>("kotlin") {
+            js(IR) {
+                browser()
+                nodejs()
+                binaries.executable()
+            }
+
+            sourceSets.maybeCreate("main").resources.srcDir(project.projectDir.resolve(File("../common/src/commonMain/resources")))
+        }
 
         configureMiniGdxDependencies(project)
 
-        project.createDir("src/jsMain/kotlin")
-        project.createDir("src/jsTest/kotlin")
-
         configureTasks(project)
+    }
+
+    private fun configureProjectRepository(project: Project) {
+        project.repositories.mavenCentral()
+        project.repositories.google()
+        // Snapshot repository. Select only our snapshot dependencies
+        project.repositories.maven {
+            url = URI("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+        }.mavenContent {
+            includeVersionByRegex("com.github.minigdx", "(.*)", "LATEST-SNAPSHOT")
+            includeVersionByRegex("com.github.minigdx.(.*)", "(.*)", "LATEST-SNAPSHOT")
+        }
+        project.repositories.mavenLocal()
+        // Will be deprecated soon... Required for dokka
+        project.repositories.jcenter()
     }
 
     private fun configureTasks(project: Project) {
         project.tasks.register("runJs") {
             group = "minigdx"
             description = "Run your game in your browser."
-            dependsOn("jsBrowserDevelopmentRun")
+            dependsOn("browserDevelopmentRun")
         }
 
         project.tasks.register("bundleJs", Zip::class.java) {
             group = "minigdx"
             description = "Create a bundle as zip."
-            dependsOn("gltf", "jsBrowserDistribution")
-            from(project.buildDir.resolve("distributions"))
+            from(project.tasks.named("browserDistribution"))
             destinationDirectory.set(project.buildDir.resolve("minigdx"))
             doLast {
                 project.logger.lifecycle("[MINIGDX] The js distribution of your game is available at: ${outputs.files.first()}")
@@ -54,15 +79,26 @@ class MiniGdxJsGradlePlugin : Plugin<Project> {
             into("build/processedResources/js/main")
         }
         project.afterEvaluate {
-            project.tasks.getByName("jsProcessResources").dependsOn(copy)
+            project.tasks.getByName("processResources").dependsOn(copy)
         }
     }
 
     private fun configureMiniGdxDependencies(project: Project) {
+        // Create custom configuration that unpack depdencies on  the js platform
+        project.configurations.create("minigdxToUnpack") {
+            setTransitive(false)
+            attributes {
+                attribute(Attribute.of("org.gradle.usage", String::class.java), "kotlin-runtime")
+            }
+        }
+
         project.afterEvaluate {
-            project.dependencies.add("commonMainImplementation", "com.github.minigdx:minigdx:${project.minigdx.version.get()}")
-            project.dependencies.add("jsMainImplementation", "com.github.minigdx:minigdx-js:${project.minigdx.version.get()}")
-            project.dependencies.add("minigdxToUnpack", "com.github.minigdx:minigdx-js:${project.minigdx.version.get()}")
+            project.dependencies.add("implementation", "com.github.minigdx:minigdx:${project.minigdx.version.get()}")
+            project.dependencies.add("implementation", "com.github.minigdx:minigdx-js:${project.minigdx.version.get()}")
+            project.dependencies.add(
+                "minigdxToUnpack",
+                "com.github.minigdx:minigdx-js:${project.minigdx.version.get()}"
+            )
         }
     }
 }
